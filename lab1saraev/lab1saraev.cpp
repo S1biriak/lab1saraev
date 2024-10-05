7d0d2f89643fb4c39d7a39b0701bde71343fcf6f
S1biriak <158584770+S1biriak@users.noreply.github.com>
2024-10-05 15:28:58 +0300

    Added menu

--- lab1saraev/lab1saraev.cpp
+++ lab1saraev/lab1saraev.cpp
@@ -15,11 +15,24 @@ struct compressor_station
 {
 	string name;
 	int workshops;
-	int workshops_in_work;
+	int workshops_in_operation;
 	int effcoef;
 };
 
 int main()
 {
-	
+    cout << "" << endl;
+    cout << " ----------- MENU ---------- " << endl;
+    cout << "" << endl;
+    cout << " 1. Add gas pipe " << endl;
+    cout << " 2. Add compression station " << endl;
+    cout << " 3. All components view " << endl;
+    cout << " 4. Edit gas pipe " << endl;
+    cout << " 5. Edit compression station " << endl;
+    cout << " 6. Save to file " << endl;
+    cout << " 7. Upload from file " << endl;
+    cout << " 0. EXIT " << endl;
+    cout << "" << endl;
+    cout << " --------------------------- " << endl;
+    cout << "" << endl;
 }
