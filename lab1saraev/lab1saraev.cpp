332d4c9c8995987cc8392607bcb739cffc8aaac6
S1biriak <158584770+S1biriak@users.noreply.github.com>
2024-10-05 15:59:26 +0300

    added switch case

--- lab1saraev/lab1saraev.cpp
+++ lab1saraev/lab1saraev.cpp
@@ -21,6 +21,7 @@ struct compressor_station
 
 int main()
 {
+    int number;
     cout << "" << endl;
     cout << " ----------- MENU ---------- " << endl;
     cout << "" << endl;
@@ -35,4 +36,34 @@ int main()
     cout << "" << endl;
     cout << " --------------------------- " << endl;
     cout << "" << endl;
+    
+    cin >> number;
+
+    switch (number) 
+    {
+    case 1:
+        cout << " 1. Add gas pipe " << endl;
+        break;
+    case 2:
+        cout << " 2. Add compression station " << endl;
+        break;
+    case 3:
+        cout << " 3. All components view " << endl;
+        break;
+    case 4:
+        cout << " 4. Edit gas pipe " << endl;
+        break;
+    case 5:
+        cout << " 5. Edit compression station " << endl;
+        break;
+    case 6:
+        cout << " 6. Save to file " << endl;
+        break;
+    case 7:
+        cout << " 7. Upload from file " << endl;
+        break;
+    case 0:
+        return false;
+        break;
+    }
 }
