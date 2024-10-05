f0126584765ab2a215019afa80ba07a114f1fce1
S1biriak <158584770+S1biriak@users.noreply.github.com>
2024-10-05 19:26:08 +0300

    added command 1

--- lab1saraev/lab1saraev.cpp
+++ lab1saraev/lab1saraev.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct pipe 
 {
-	string name;
+	string name="no";
 	double length;
 	int diameter;
 	bool repair;
@@ -13,57 +13,108 @@ struct pipe
 
 struct compressor_station 
 {
-	string name;
+	string name="no";
 	int workshops;
 	int workshops_in_operation;
 	int effcoef;
 };
 
+pipe add_pipe() 
+{
+    pipe new_pipe;
+    cout << " Enter the pipe name " << endl;
+    cin >> new_pipe.name;
+    cout << " Enter the length of the pipe " << endl;
+    cin >> new_pipe.length;
+    cout << " Enter the diameter of the pipe " << endl;
+    cin >> new_pipe.diameter;
+    cout << " Ennter the repair of the pipe " << endl;
+    cin >> new_pipe.repair;
+    return new_pipe;
+}
+
+void output_pipe(pipe new_pipe) 
+{
+    if (new_pipe.name == "no") 
+    {
+        cout << " No new pipe " << endl;
+    }
+    else 
+    {
+        cout << " Info of pipes" << endl;
+        cout << " 1. Name: " << "" << new_pipe.name << endl;
+        cout << " 2. Length: " << "" << new_pipe.length << endl;
+        cout << " 3. Diameter: " << "" << new_pipe.diameter << endl;
+        cout << " 4. Repair: " << "" << new_pipe.repair << endl;
+    }
+}
+
+void edit_pipe(pipe new_pipe)
+{
+    if (new_pipe.name == "no") 
+    {
+        cout << " No new pipe " << endl;
+    }
+    else 
+    {
+        cout << " Editing the under repair attribute " << endl;;
+        cin >> new_pipe.repair;
+    }
+}
+
+
 int main()
 {
+    pipe new_pipe;
     int number;
-    cout << "" << endl;
-    cout << " ----------- MENU ---------- " << endl;
-    cout << "" << endl;
-    cout << " 1. Add gas pipe " << endl;
-    cout << " 2. Add compression station " << endl;
-    cout << " 3. All components view " << endl;
-    cout << " 4. Edit gas pipe " << endl;
-    cout << " 5. Edit compression station " << endl;
-    cout << " 6. Save to file " << endl;
-    cout << " 7. Upload from file " << endl;
-    cout << " 0. EXIT " << endl;
-    cout << "" << endl;
-    cout << " --------------------------- " << endl;
-    cout << "" << endl;
-    
-    cin >> number;
-
-    switch (number) 
+    while (true) 
     {
-    case 1:
+        cout << "" << endl;
+        cout << " ----------- MENU ---------- " << endl;
+        cout << "" << endl;
         cout << " 1. Add gas pipe " << endl;
-        break;
-    case 2:
         cout << " 2. Add compression station " << endl;
-        break;
-    case 3:
         cout << " 3. All components view " << endl;
-        break;
-    case 4:
         cout << " 4. Edit gas pipe " << endl;
-        break;
-    case 5:
         cout << " 5. Edit compression station " << endl;
-        break;
-    case 6:
         cout << " 6. Save to file " << endl;
-        break;
-    case 7:
         cout << " 7. Upload from file " << endl;
-        break;
-    case 0:
-        return false;
-        break;
+        cout << " 0. EXIT " << endl;
+        cout << "" << endl;
+        cout << " --------------------------- " << endl;
+        cout << "" << endl;
+        cout << " Enter the number " << endl;
+        cin >> number;
+
+        switch (number)
+        {
+        case 1:
+            cout << " 1. Add gas pipe " << endl;
+            new_pipe = add_pipe();
+            output_pipe(new_pipe);
+            break;
+        case 2:
+            cout << " 2. Add compression station " << endl;
+            break;
+        case 3:
+            cout << " 3. All components view " << endl;
+            break;
+        case 4:
+            cout << " 4. Edit gas pipe " << endl;
+            break;
+        case 5:
+            cout << " 5. Edit compression station " << endl;
+            break;
+        case 6:
+            cout << " 6. Save to file " << endl;
+            break;
+        case 7:
+            cout << " 7. Upload from file " << endl;
+            break;
+        case 0:
+            return false;
+            break;
+        }
     }
+    
 }
