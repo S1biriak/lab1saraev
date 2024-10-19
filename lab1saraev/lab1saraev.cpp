9a791c61b78abea58a0f0c48989ecad819b3c620
S1biriak <158584770+S1biriak@users.noreply.github.com>
2024-10-19 16:11:34 +0300

    added command load ans save

--- lab1saraev/lab1saraev.cpp
+++ lab1saraev/lab1saraev.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -22,14 +23,15 @@ struct cs
 pipe add_pipe() 
 {
     pipe new_pipe;
-    cout << " Enter the pipe name " << endl;
+    cout << " Enter the pipe name: ";
     cin >> new_pipe.name;
-    cout << " Enter the length of the pipe " << endl;
+    cout << " Enter the length of the pipe: ";
     cin >> new_pipe.length;
-    cout << " Enter the diameter of the pipe " << endl;
+    cout << " Enter the diameter of the pipe: ";
     cin >> new_pipe.diameter;
-    cout << " Enter the repair of the pipe " << endl;
+    cout << " Enter the repair of the pipe: ";
     cin >> new_pipe.repair;
+    cout << "" << endl;
     return new_pipe;
 }
 
@@ -62,17 +64,41 @@ void edit_pipe(pipe new_pipe)
     }
 }
 
+void save_pipe(const pipe& pipe)
+{
+    ofstream fout("info.txt");
+    if (pipe.name == "no")
+    {
+        fout << " No pipe " << endl;
+    }
+    else
+    {
+        fout << " Info of pipes" << endl;
+        fout << pipe.name << endl;
+        fout << pipe.length << endl;
+        fout << pipe.diameter << endl;
+        fout << pipe.repair << endl;
+    }
+    fout.close();
+}
+
+void load_pipe(pipe& pipe)
+{
+    ifstream fin("info.txt");
+}
+
 cs add_cs()
 {
     cs new_cs;
-    cout << " Enter the cs name " << endl;
+    cout << " Enter the cs name: ";
     cin >> new_cs.name;
-    cout << " Enter the workshops of the cs " << endl;
+    cout << " Enter the workshops of the cs: ";
     cin >> new_cs.workshops;
-    cout << " Enter the workshops_in_operation of the cs " << endl;
+    cout << " Enter the workshops_in_operation of the cs: ";
     cin >> new_cs.workshops_in_operation;
-    cout << " Enter the effcoef of the cs " << endl;
+    cout << " Enter the effcoef of the cs: ";
     cin >> new_cs.effcoef;
+    cout << "" << endl;
     return new_cs;
 }
 
@@ -104,6 +130,31 @@ void edit_cs(cs new_cs)
         cin >> new_cs.workshops_in_operation;
     }
 }
+
+void save_cs(const cs& cs)
+{
+    ofstream fout("info.txt");
+    if (cs.name == "no")
+    {
+        fout << " No station " << endl;
+    }
+    else
+    {
+        fout << " Info of station" << endl;
+        fout << cs.name << endl;
+        fout << cs.workshops << endl;
+        fout << cs.workshops_in_operation << endl;
+        fout << cs.effcoef << endl;
+    }
+    fout.close();
+}
+
+void load_cs(cs& cs)
+{
+    ifstream fin("info.txt");
+}
+
+
 int main()
 {
     pipe new_pipe;
@@ -157,15 +208,19 @@ int main()
             break;
         case 6:
             cout << " 6.1. Save gas pipe to file " << endl;
+            save_pipe(new_pipe);
             break;
         case 7:
             cout << " 7.1. Upload gas pipe from file " << endl;
+            load_pipe(new_pipe);
             break;
         case 8:
             cout << " 6.2. Save compression station to file " << endl;
+            save_cs(new_cs);
             break;
         case 9:
             cout << " 7.2. Upload compression station from file " << endl;
+            load_cs(new_cs);
             break;
         case 0:
             return false;
